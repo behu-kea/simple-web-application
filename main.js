8213ee46b098098ad91deb9207afaf18082aec81
Benjamin Hughes <behu@kea.dk>
2022-08-08 09:28:49 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// No need to understand this!!!
+const pElement = document.createElement("p");
+document.body.appendChild(pElement);
+// No need to understand this!!!
+
+pElement.innerHTML = 2 + 7;
